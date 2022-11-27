8e9784b654a93b2702a91f63da77be4ba7f8eac7
golemeyes <rattaphol.y@netdesignhost.com>
2022-11-27 23:37:55 +0700

    add new project

--- Modules/Mwz/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('mwz::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('mwz.name') !!}
-    </p>
-@endsection
