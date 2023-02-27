f17af9e648e5c39e83c4c8db0e6524cfea91239a
golemeyes <rattaphol.y@netdesignhost.com>
2023-02-27 12:36:18 +0700

    add new port

---
+++ js/graphic-compo.js
@@ -0,0 +1,216 @@
+class Allgraphic extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <ads-banner></ads-banner>
+        `;
+  }
+}
+
+class Adsbanner extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+
+        <div class="g-port-list">
+            <ul>
+
+                <li>
+                    <a href="images/ads/g-4.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-4.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/g-17.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-17.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/g-19.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-19.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/g-10.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-10.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/g-9.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-9.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+               
+
+                
+
+                <li>
+                    <a href="images/ads/g-18.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-18.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+               
+
+                <li>
+                    <a href="images/ads/g-20.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/g-20.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/ads-1.jpg">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-1.jpg">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE OA SERIVCE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/ads-2.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-2.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " CLOUD SERVER SERVICE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                 <li>
+                    <a href="images/ads/ads-3.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-3.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " CLOUD DEDICATE SERVICE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                 <li>
+                    <a href="images/ads/ads-4.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-4.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " SEO HOSTING "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/ads-5.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-5.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " SEO HOSTING "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/ads-6.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-6.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " WEBSITE PACKAGE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="images/ads/ads-7.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-7.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " LINE TEMPLATE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                 <li>
+                    <a href="images/ads/ads-8.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-8.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " WEBSITE PACKAGE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                 <li>
+                    <a href="images/ads/ads-9.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-9.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " WEBSITE PACKAGE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+
+                 <li>
+                    <a href="images/ads/ads-10.png">
+                        <div class="inner">
+                        <img class="w-100" src="images/ads/ads-10.png">
+                        <div class="detail">
+                            <h5>ADS BANNER " WEBSITE PACKAGE "</h5>
+                        </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        `;
+  }
+}
+
+customElements.define("ads-banner", Adsbanner);
+customElements.define("all-graphic", Allgraphic);
---
+++ js/script.js
@@ -0,0 +1,65 @@
+// componente
+
+
+
+/* Menu Toggle */
+
+$(document).ready(function(){
+   $("#toggler").click(function(){
+     $("#toggle-nav").fadeToggle(500);
+   });
+ });
+
+ $('.menu-toggle').on('click', function() {
+   $('.wrapper').toggleClass('menu--is-revealed');
+ });
+
+ /* Half Page Responsive */
+
+ $(document).ready(function($) {
+   var alterClass = function() {
+     var ww = document.body.clientWidth;
+     if (ww < 1199) {
+       $('.LeftContent ').removeClass('LeftContent');
+       $('.RightContent ').removeClass('RightContent');
+     } else if (ww >= 1200) {
+       $('.LeftContent').addClass('LeftContent');
+       $('.RightContent ').addClass('RightContent');
+     };
+   };
+ });
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain attribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+
+
+
+
+
